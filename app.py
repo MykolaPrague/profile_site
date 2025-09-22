017d68708132cacb525ca3d8d3d472dd87364c42
MykolaPrague <144772212+MykolaPrague@users.noreply.github.com>
2025-09-22 03:39:26 +0200

    +переклад і туглер

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from flask_babel import Babel, _, get_locale  # ⬅ додали get_locale
 
 app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = 'uk'
+app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
+
+def select_locale():
+    return request.args.get('lang') or 'uk'
+
+babel = Babel(app, locale_selector=select_locale)
+
+# ⬅ зробимо змінну доступною в усіх шаблонах
+@app.context_processor
+def inject_locale():
+    # get_locale() повертає Babel Locale; перетворимо на рядок ('uk', 'en', 'cs', ...)
+    return {'current_locale': str(get_locale())}
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html", name="Mykola Shyshka")
 
 if __name__ == "__main__":
